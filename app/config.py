48eb762e4aea0f0e80e83d1d433bccfc7ea2cf5d
kaidenvlr <kaidenvlr@gmail.com>
2023-10-12 23:02:03 +0600

    asadal token

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PG_URL: PostgresDsn = os.getenv("PG_URL")
+    ASADAL_TOKEN: str = os.getenv("ASADAL_TOKEN")
 
 
 settings = Settings()
