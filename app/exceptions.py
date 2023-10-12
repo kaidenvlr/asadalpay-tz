d73f78b3f8aa9eacd6fddab6d6cae2b70e78bdc7
kaidenvlr <kaidenvlr@gmail.com>
2023-10-12 23:03:16 +0600

    raise 422 error

--- app/exceptions.py
+++ app/exceptions.py
@@ -9,3 +9,9 @@ class NotFoundException(HTTPException):
 class BadRequestException(HTTPException):
     def __init__(self, msg: str = ""):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg if msg else "Bad request")
+
+
+class NonProcessableEntityException(HTTPException):
+    def __init__(self, msg: str = ""):
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail=msg if msg else "Unprocessble Entity")
