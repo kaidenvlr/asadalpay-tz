1dadcfabc811b22983129ba5b5213db82f7384cb
kaidenvlr <kaidenvlr@gmail.com>
2023-10-12 23:02:49 +0600

    include order router

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.api.v1.item import router as item_router
+from app.api.v1.order import router as order_router
 
 router = APIRouter(prefix='/api/v1')
 
 router.include_router(item_router)
+router.include_router(order_router)
