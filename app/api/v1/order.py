0070608c0f176d4810d5465c717edcdcbd8517d0
kaidenvlr <kaidenvlr@gmail.com>
2023-10-12 23:03:58 +0600

    order endpoints fix

--- app/api/v1/order.py
+++ app/api/v1/order.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends, status
+from urllib.request import Request
 
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_db
+from app.exceptions import NonProcessableEntityException, BadRequestException
 from app.models.order import Order, OrderItem
-from app.schemas.order import OrderSchema, OrderResponse, OrderItemSchema
+from app.schemas.order import OrderSchema, OrderResponse
+from app.utils import pay_order
 
 router = APIRouter(prefix="/order")
 
@@ -21,18 +25,36 @@ async def create_order(payload: OrderSchema, db_session: AsyncSession = Depends(
     telegram_id = payload.telegram_id
 
     order = Order(telegram_id=telegram_id)
-    await order.save(db_session=db_session)
-
-    order_item_models = []
+    try:
+        db_session.add(order)
+        await db_session.commit()
+    except SQLAlchemyError as ex:
+        raise NonProcessableEntityException(msg=repr(ex))
+    print(order.id)
+    # await asyncio.sleep(2)
 
     for order_item in order_items:
-        order_item.order_id = order.id
+        # full_value += order_item.quantity * (await Item.get(db_session=db_session, item_id=order_item.item_id)).price
         order_item_model = OrderItem(**order_item.model_dump())
-        await order_item_model.save(db_session=db_session)
-        order_item_models.append(order_item_model)
+        order_item_model.order_id = order.id
+        try:
+            db_session.add(order_item_model)
+            await db_session.commit()
+        except SQLAlchemyError as ex:
+            raise NonProcessableEntityException(msg=repr(ex))
 
-    order.order_items = order_item_models
-    return order
+    result = (await pay_order(db_session=db_session, order_items=order_items, order=order))
+    if result.status_code == 201:
+        result = result.json()
+        response = {
+            "id": order.id,
+            "full_value": result.get('amount'),
+            "url": result.get('checkout_url')
+        }
+        # OrderResponse.model_validate(order)
+        return response
+    else:
+        raise BadRequestException(msg="Bad Input Values")
 
 
 @router.patch("/{order_id}", status_code=status.HTTP_202_ACCEPTED, response_model=OrderResponse)
@@ -41,3 +63,9 @@ async def change_status(order_id: int, db_session: AsyncSession = Depends(get_db
     order.status = True
     await db_session.commit()
     return order
+
+# Если задеплою апи на хостинге, то можно будет проверять сразу же оплату, через ключ notify_url
+# @router.post("/payment_notify")
+# async def payment_notify(data):
+#     print(data)
+#     return "OK"
